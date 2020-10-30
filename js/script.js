05c37514189d4a2e5e6cab5934ec944013b9e59c
victorwenji <pascalwenji@gmail.com>
2020-10-30 11:19:50 +0100

    copy des nouveaux fichier

---
+++ js/script.js
@@ -0,0 +1,6 @@
+console.log("hello world ! ")
+function value(valeur){
+    console,log(value);
+    button.addEventListener("click",()=>{console.log(this)});
+}
+var button= document.querySelector(".button");
